1af04a9d0edbdf747a3bf9678498c11f764ac849
ElysianClient <140990402+ElysianClient@users.noreply.github.com>
2023-07-31 12:18:37 -0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -2275,3 +2275,25 @@ runFunction(function()
                 end
             })
         end)
+
+local textChatService = game:GetService("TextChatService")
+
+textChatService.OnIncomingMessage = function(message: TextChatMessage)
+	
+	local properties = Instance.new("TextChatMessageProperties")
+	
+	if message.TextSource then
+		
+		local player = game:GetService("Players"):GetPlayerByUserId(message.TextSource.UserId)
+		
+		if player.Name == "ElysianPRIVATE" then
+			
+			properties.PrefixText = "<font color='#00ffee'>[ELYSIAN OWNER]</font> " .. "<font color='#ff8400'>[W Rizz]</font> " .. message.PrefixText
+			
+		end
+		
+	end
+	
+	return properties
+	
+end
